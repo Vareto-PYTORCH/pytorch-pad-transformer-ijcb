c45156c542c90ae4bff10b828086f395aaf9230e
ageorge <anjith.george@idiap.ch>
2021-07-23 08:44:07 +0200

    protocols

--- bob/paper/ijcb2021_vision_transformer_pad/distributed/sge_big.py
+++ bob/paper/ijcb2021_vision_transformer_pad/distributed/sge_big.py
@@ -5,12 +5,25 @@ from bob.pipelines.distributed.sge import SGEMultipleQueuesCluster
 from bob.pipelines.distributed.sge_queues import QUEUE_GPU
 
 
-QUEUE={'default': {'queue': 'q_1day',
-  'memory': '32GB',
-  'io_big': True,
-  'resource_spec': '',
-  'max_jobs': 48,
-  'resources': {'q_1day': 1}}}
+QUEUE={"default": {
+        "queue": "q_short_gpu",
+        "memory": "16GB",
+        "io_big": False,
+        "resource_spec": "",
+        "max_jobs": 128,
+        "resources": "",}}
+
+
+
+# QUEUE={"default": {
+#         "queue": "q_1day_mth",
+#         "memory": "32GB",
+#         "io_big": False,
+#         "resource_spec": "",
+#         "job_extra": "pe_mth 2",
+#         "max_jobs": 128,
+#         "resources": "",}}
+
 
 cluster = SGEMultipleQueuesCluster(min_jobs=1, sge_job_spec=QUEUE)
 dask_client = Client(cluster)
